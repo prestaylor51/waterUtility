abfbc3688a3d4879857987c90dd97cab04a15af1
prestaylor51 <prestaylor51@gmail.com>
2017-12-21 00:50:04 -0700

    crude server

---
+++ index.js
@@ -0,0 +1,21 @@
+/*************************************
+* File: INDEX.JS
+*************************************/
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+	console.log(request.url);
+	response.end('Hello Server!');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+
+	if (err) {
+		return console.log('something went wrong!', err)
+	}
+
+	console.log("Server is listening on port " + port);
+})
